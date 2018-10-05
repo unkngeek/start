6cfeb23592124a8c9659f8a443f408d8a98a98c0
unkngeek <unkngeek@gmail.com>
2018-10-05 18:26:02 +0300

    first commit to GH

---
+++ server.js
@@ -0,0 +1,13 @@
+var User = require('./user');
+
+function run() {
+    var vasya = new User ("Вася");
+    var petya = new User ("Петя");
+    vasya.hello(petya);
+}
+
+if (module.parent) {
+    exports.run = run;
+}else{
+    run();
+}
---
+++ user/index.js
@@ -0,0 +1,14 @@
+//exports
+// global
+var phrases = require('./ru');
+
+function User(name) {
+    this.name = name;
+}
+
+User.prototype.hello = function (who) {
+    // ...
+    console.log(phrases.Hello+ ", " + who.name);
+};
+
+module.exports = User;
